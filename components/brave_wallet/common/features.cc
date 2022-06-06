40892a5e76e17609936724d5e15e6c0962d2e290
Serg <serg.zhukovsky@gmail.com>
2022-06-06 13:37:43 -0400

    Enables Android DApps support by default

--- components/brave_wallet/common/features.cc
+++ components/brave_wallet/common/features.cc
@@ -30,14 +30,8 @@ const base::Feature kBraveWalletSolanaProviderFeature{
 const base::Feature kBraveWalletEditKnownNetworksFeature{
     "BraveWalletEditKnownNetworks", base::FEATURE_DISABLED_BY_DEFAULT};
 
-const base::Feature kBraveWalletDappsSupportFeature {
-  "BraveWalletDappsSupport",
-#if BUILDFLAG(IS_ANDROID)
-      base::FEATURE_DISABLED_BY_DEFAULT
-#else
-      base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-};
+const base::Feature kBraveWalletDappsSupportFeature{
+    "BraveWalletDappsSupport", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace brave_wallet

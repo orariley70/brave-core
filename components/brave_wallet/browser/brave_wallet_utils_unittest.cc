3c2ba0ddf5954248e3d3a162a453131207a40eeb
oisupov <oisupov@brave.com>
2022-06-10 22:04:53 +0700

    Hide chooser for filenet Resoves https://github.com/brave/brave-browser/issues/23367

--- components/brave_wallet/browser/brave_wallet_utils_unittest.cc
+++ components/brave_wallet/browser/brave_wallet_utils_unittest.cc
@@ -974,7 +974,7 @@ TEST_F(BraveWalletUtilsUnitTest, GetChain) {
   EXPECT_EQ(GetChain(&prefs, "f", mojom::CoinType::FIL), fil_mainnet.Clone());
 }
 
-TEST(BraveWalletUtilsUnitTest, GetAllKnownEthNetworkIds) {
+TEST_F(BraveWalletUtilsUnitTest, GetAllKnownEthNetworkIds) {
   const std::vector<std::string> expected_network_ids(
       {"mainnet", mojom::kPolygonMainnetChainId,
        mojom::kBinanceSmartChainMainnetChainId, mojom::kCeloMainnetChainId,
